01ba0ddb2d14bf98428256e256574479176ab190
Paul Walger <metaxy@walger.name>
2015-01-20 11:04:36 +0100

    update

--- main_blp.cpp
+++ main_blp.cpp
@@ -4,6 +4,7 @@ int main()
 {
     Graph g = Common::graphFromStdin();
     //Graph g = Common::graphFromFile("/home/paul/Cloud/studium/Cluster/programs/material/ce-data/test014.graph");
+    Graph g = Common::graphFromFile("../../programs/material/ce-data/test014.graph");
 
     StateBlp s0(g);
     s0.solve();
